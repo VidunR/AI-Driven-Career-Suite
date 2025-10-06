83372e5b9f00c3007ab6138548fc97e1505e32ff
Luchintha <luchinthaa@gmail.com>
2025-10-06 16:35:46 +0530

    Complete App Code

--- Backend/app.js
+++ Backend/app.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Call the Layout
+// Call the Layout router
 app.use('/applayout', applayoutRouter);
 
 // Calls the auth router
